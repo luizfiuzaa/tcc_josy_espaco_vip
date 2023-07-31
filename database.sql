151a4af192daba30917c94d8eeac57b9ce56baf4
MarlonVBP <marlonvicctor13@gmail.com>
2023-07-31 09:08:59 -0300

    add novas tabelas

--- database.sql
+++ database.sql
@@ -23,5 +23,34 @@ CREATE TABLE `agendamento_servicos`(
 DROP TABLE IF EXISTS `servico`;
 CREATE TABLE `servico`(
   `id_servico` INT NOT NULL AUTO_INCREMENT,
-  `duracao` TI
+  `duracao` INT NOT NULL
 );
+
+DROP TABLE IF EXISTS `admin_Mensagem`;
+CREATE TABLE `admin_Mensagem` (
+    `Id_admin_Msg` INT primary Key AUTO_INCREMENT,
+    `FK_Id_Msg` INT NOT NUll,
+    `FK_Id_Admin` INT NOT NUll
+)
+
+DROP TABLE IF EXISTS `Mensagem`;
+CREATE TABLE `Mensagem` (
+    `Id_Msg` INT primary Key AUTO_INCREMENT,
+    `Desc_Msg` VARCHAR(255) NOT NUll
+)
+
+DROP TABLE IF EXISTS `Cliente`;
+CREATE TABLE `Cliente` (
+    `Id_Cliente` INT primary Key AUTO_INCREMENT,
+    `Cliente_nome` VARCHAR(50) NOT NUll,
+    `Cliente_tel` VARCHAR(12) NOT NUll,
+    `Cliente_email` VARCHAR(50) NOT NUll,
+    `Cliente_senha` VARCHAR(255) NOT NUll
+)
+
+DROP TABLE IF EXISTS `Mensagem_cliente`;
+CREATE TABLE `Mensagem_cliente` (
+    `Id_Msg_Cli` INT primary Key,
+    `Fk_id_cli` INT NOT NUll,
+    `Fk_id_Msg` INT NOT NUll
+)
