f63d7649d0240d7956cd7d6eb33810aca5c792f1
nomloki <luizffreitas2006@gmail.com>
2023-07-31 09:20:47 -0300

    colocando primary key's

--- database.sql
+++ database.sql
@@ -5,52 +5,60 @@ DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin`(
   `id_admin` INT NOT NULL AUTO_INCREMENT,
   `email_admin` VARCHAR(50) NOT NULL,
-  `senha_admin` VARCHAR(50) NOT NULL
+  `senha_admin` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`id_amdin`)
 )
 DROP TABLE IF EXISTS `agendamento`;
 CREATE TABLE `agendamento`(
   `id_agendamento` INT NOT NULL AUTO_INCREMENT,
   `data_inicio` DATETIME NOT NULL,
-  `data_fim` DATETIME NOT NULL
+  `data_fim` DATETIME NOT NULL,
   -- foreignKeys
+  PRIMARY KEY (`id_agendamento`)
 );
 DROP TABLE IF EXISTS `agendamento_servicos`;
 CREATE TABLE `agendamento_servicos`(
-  `id_agend_serv` INT NOT NULL AUTO_INCREMENT
+  `id_agend_serv` INT NOT NULL, AUTO_INCREMENT
   -- foreignKeys
+  PRIMARY KEY (`id_agend_serv`)
 );
 
 DROP TABLE IF EXISTS `servico`;
 CREATE TABLE `servico`(
   `id_servico` INT NOT NULL AUTO_INCREMENT,
-  `duracao` INT NOT NULL
+  `duracao` INT NOT NULL,
+  PRIMARY KEY (`servico`)
 );
 
 DROP TABLE IF EXISTS `admin_Mensagem`;
 CREATE TABLE `admin_Mensagem` (
-    `Id_admin_Msg` INT primary Key AUTO_INCREMENT,
+    `id_admin_msg` INT primary Key AUTO_INCREMENT,
     `FK_Id_Msg` INT NOT NUll,
-    `FK_Id_Admin` INT NOT NUll
+    `FK_Id_Admin` INT NOT NUll,
+  PRIMARY KEY (`id_admin_msg`)
 )
 
 DROP TABLE IF EXISTS `Mensagem`;
 CREATE TABLE `Mensagem` (
-    `Id_Msg` INT primary Key AUTO_INCREMENT,
-    `Desc_Msg` VARCHAR(255) NOT NUll
+    `id_msg` INT primary Key AUTO_INCREMENT,
+    `Desc_Msg` VARCHAR(255) NOT NUll,
+  PRIMARY KEY (`id_msg`)
 )
 
 DROP TABLE IF EXISTS `Cliente`;
 CREATE TABLE `Cliente` (
-    `Id_Cliente` INT primary Key AUTO_INCREMENT,
+    `id_cliente` INT primary Key AUTO_INCREMENT,
     `Cliente_nome` VARCHAR(50) NOT NUll,
     `Cliente_tel` VARCHAR(12) NOT NUll,
     `Cliente_email` VARCHAR(50) NOT NUll,
-    `Cliente_senha` VARCHAR(255) NOT NUll
+    `Cliente_senha` VARCHAR(255) NOT NUll,
+  PRIMARY KEY (`id_cliente`)
 )
 
 DROP TABLE IF EXISTS `Mensagem_cliente`;
 CREATE TABLE `Mensagem_cliente` (
-    `Id_Msg_Cli` INT primary Key,
+    `id_msg_cli` INT primary Key,
     `Fk_id_cli` INT NOT NUll,
     `Fk_id_Msg` INT NOT NUll
+  PRIMARY KEY (`id_msg_cli`)
 )
