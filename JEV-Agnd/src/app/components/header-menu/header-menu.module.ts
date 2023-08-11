c060f13bb49c0e0fae473fc81ade93969e184246
MarlonPassos17 <galeriademarlon@gmail.com>
2023-08-11 13:50:25 -0300

    create headerMenu component

---
+++ JEV-Agnd/src/app/components/header-menu/header-menu.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderMenuComponent } from './header-menu.component';
+
+@NgModule({
+  declarations: [
+    HeaderMenuComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HeaderMenuComponent
+  ]
+})
+export class CarouselModule { }
