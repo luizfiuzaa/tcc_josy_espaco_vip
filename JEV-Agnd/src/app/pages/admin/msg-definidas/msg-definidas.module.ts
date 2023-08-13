f8d752683b76d89a3bd76554f3d75013e17c5dd6
Kits93 <joaomarins93@gmail.com>
2023-08-13 01:42:09 -0300

    progresso com a pagina de mensagens

--- JEV-Agnd/src/app/pages/admin/msg-definidas/msg-definidas.module.ts
+++ JEV-Agnd/src/app/pages/admin/msg-definidas/msg-definidas.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { MsgDefinidasPageRoutingModule } from './msg-definidas-routing.module';
 
 import { MsgDefinidasPage } from './msg-definidas.page';
+import { HeaderMenuModule } from 'src/app/components/header-menu/header-menu.module'; 
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    HeaderMenuModule,
     MsgDefinidasPageRoutingModule
   ],
   declarations: [MsgDefinidasPage]
