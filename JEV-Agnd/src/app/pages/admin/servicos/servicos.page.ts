54bdccf1515c266eda0a26f25865d1fa153b6cad
MarlonPassos17 <galeriademarlon@gmail.com>
2023-08-17 01:52:19 -0300

    alguns ajustes

--- JEV-Agnd/src/app/pages/admin/servicos/servicos.page.ts
+++ JEV-Agnd/src/app/pages/admin/servicos/servicos.page.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { CheckboxCustomEvent } from '@ionic/angular';
 
 interface CardDados {
   tituloCard: string;
@@ -15,8 +16,39 @@ interface CardDados {
 export class ServicosPage implements OnInit {
 
   @Input() Card_Dados: CardDados[] = []
+  canDismiss = false;
+
+  presentingElement: any;
 
   ngOnInit() {
+    this.presentingElement = document.querySelector('.ion-page') as HTMLElement;
+  }
+
+  modalOpen1 = false;
+
+  setOpen1(isOpen: boolean) {
+    this.modalOpen1 = isOpen;
+  }
+
+  // modal 2
+  modalOpen2 = false;
+
+  setOpen2(isOpen: boolean) {
+    this.modalOpen2 = isOpen;
+  }
+
+  // modal 3
+  modalOpen3 = false;
+
+  setOpen3(isOpen: boolean) {
+    this.modalOpen3 = isOpen;
+  }
+
+  // modal 4
+  modalOpen4 = false;
+
+  setOpen4(isOpen: boolean) {
+    this.modalOpen4 = isOpen;
   }
 
   CardDados: any[] = [
