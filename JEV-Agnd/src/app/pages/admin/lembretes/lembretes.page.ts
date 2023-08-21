7db682299932b30a05c6bc30042a7c62140369b5
nomloki <luizffreitas2006@gmail.com>
2023-08-20 22:17:35 -0300

    major update: remake tela de lembretes

--- JEV-Agnd/src/app/pages/admin/lembretes/lembretes.page.ts
+++ JEV-Agnd/src/app/pages/admin/lembretes/lembretes.page.ts
@@ -1,10 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
-
 interface Lembretes {
   horarioLembrete: string;
   conteudoLembrete: string;
 }
-
 @Component({
   selector: 'app-lembretes',
   templateUrl: './lembretes.page.html',
@@ -40,47 +38,47 @@ export class LembretesPage implements OnInit {
 
   Lembretes: any[] = [
     {
-      horarioLembrete: '10:00',
+      horarioLembrete: '10:00', dataLembrete: 'DD/MM/AAAA',
       conteudoLembrete: 'Você tem um agendamento marcado as 10:30'
     },
     {
-      horarioLembrete: '10:00',
+      horarioLembrete: '10:00', dataLembrete: 'DD/MM/AAAA',
       conteudoLembrete: 'Você tem um agendamento marcado as 10:30'
     },
     {
-      horarioLembrete: '10:00',
+      horarioLembrete: '10:00', dataLembrete: 'DD/MM/AAAA',
       conteudoLembrete: 'Você tem um agendamento marcado as 10:30'
     },
     {
-      horarioLembrete: '10:00',
+      horarioLembrete: '10:00', dataLembrete: 'DD/MM/AAAA',
       conteudoLembrete: 'Você tem um agendamento marcado as 10:30'
     },
     {
-      horarioLembrete: '10:00',
+      horarioLembrete: '10:00', dataLembrete: 'DD/MM/AAAA',
       conteudoLembrete: 'Você tem um agendamento marcado as 10:30'
     },
     {
-      horarioLembrete: '10:00',
+      horarioLembrete: '10:00', dataLembrete: 'DD/MM/AAAA',
       conteudoLembrete: 'Você tem um agendamento marcado as 10:30'
     },
     {
-      horarioLembrete: '10:00',
+      horarioLembrete: '10:00', dataLembrete: 'DD/MM/AAAA',
       conteudoLembrete: 'Você tem um agendamento marcado as 10:30'
     },
     {
-      horarioLembrete: '10:00',
+      horarioLembrete: '10:00', dataLembrete: 'DD/MM/AAAA',
       conteudoLembrete: 'Você tem um agendamento marcado as 10:30'
     },
     {
-      horarioLembrete: '11:00',
+      horarioLembrete: '11:00', dataLembrete: 'DD/MM/AAAA',
       conteudoLembrete: 'Você tem um agendamento marcado as 12:00'
     },
     {
-      horarioLembrete: '13:00',
+      horarioLembrete: '13:00', dataLembrete: 'DD/MM/AAAA',
       conteudoLembrete: 'Você tem um agendamento marcado as 14:00'
     },
     {
-      horarioLembrete: '17:00',
+      horarioLembrete: '17:00', dataLembrete: 'DD/MM/AAAA',
       conteudoLembrete: 'Você tem um agendamento marcado as 10:00'
     }
   ];
