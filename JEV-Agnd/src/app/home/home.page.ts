69f0b89ddf5e8d1dba4a609e17432d4f728b0b14
nomloki <luizffreitas2006@gmail.com>
2023-08-21 00:19:50 -0300

    major update: homepage consome dados do typescript

--- JEV-Agnd/src/app/home/home.page.ts
+++ JEV-Agnd/src/app/home/home.page.ts
@@ -9,4 +9,27 @@ export class HomePage {
 
   constructor() {}
 
+  Agendamentos: any[] =[
+  {
+    status:'Confirmado',
+    horarioInicio:'9:00', horarioFim:'10:30',
+    cliente:'Laura Peneira',
+    servico:'Cabelo, Manicure, Pedicure',
+    metodoPagamento:'Pix', preco:'150.00',
+  },
+  {
+    status:'Esperando confirmação',
+    horarioInicio:'11:00', horarioFim:'12:30',
+    cliente:'Josefina Chapéu',
+    servico:'Cabelo',
+    metodoPagamento:'Dinheiro', preco:'50.00',
+  },
+  {
+    status:'Esperando confirmação',
+    horarioInicio:'14:20', horarioFim:'15:20',
+    cliente:'Claudinha Buxexa',
+    servico:'Manicure, Pedicure',
+    metodoPagamento:'Débito', preco:'100.00',
+  },
+]
 }
