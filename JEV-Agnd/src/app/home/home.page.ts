9d263e35f8c4f8cad1ffd04f489a2fabb5403529
MarlonPassos17 <galeriademarlon@gmail.com>
2023-08-24 20:13:38 -0300

    upadate modal

--- JEV-Agnd/src/app/home/home.page.ts
+++ JEV-Agnd/src/app/home/home.page.ts
@@ -7,74 +7,54 @@ import { ActionSheetController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  presentingElement: any = undefined;
+  constructor() { }
 
-  constructor(private actionSheetCtrl: ActionSheetController) {}
+  Agendamentos: any[] = [
+    {
+      status: 'Confirmado',
+      horarioInicio: '9:00', horarioFim: '10:30',
+      cliente: 'Laura Peneira',
+      servico: 'Cabelo, Manicure, Pedicure',
+      metodoPagamento: 'Pix',
+      preco: '150.00',
+    },
+    {
+      status: 'Esperando confirmação',
+      horarioInicio: '11:00', horarioFim: '12:30',
+      cliente: 'Josefina Chapéu',
+      servico: 'Cabelo',
+      metodoPagamento: 'Dinheiro',
+      preco: '50.00',
+    },
+    {
+      status: 'Esperando confirmação',
+      horarioInicio: '14:20', horarioFim: '15:20',
+      cliente: 'Claudinha Buxexa',
+      servico: 'Manicure, Pedicure',
+      metodoPagamento: 'Débito',
+      preco: '100.00',
+    },
+  ]
 
-  Agendamentos: any[] =[
-  {
-    status:'Confirmado',
-    horarioInicio:'9:00', horarioFim:'10:30',
-    cliente:'Laura Peneira',
-    servico:'Cabelo, Manicure, Pedicure',
-    metodoPagamento:'Pix',
-    preco:'150.00',
-  },
-  {
-    status:'Esperando confirmação',
-    horarioInicio:'11:00', horarioFim:'12:30',
-    cliente:'Josefina Chapéu',
-    servico:'Cabelo',
-    metodoPagamento:'Dinheiro',
-    preco:'50.00',
-  },
-  {
-    status:'Esperando confirmação',
-    horarioInicio:'14:20', horarioFim:'15:20',
-    cliente:'Claudinha Buxexa',
-    servico:'Manicure, Pedicure',
-    metodoPagamento:'Débito',
-    preco:'100.00',
-  },
-]
+  ngOnInit() {
 
-ngOnInit() {
-  this.presentingElement = document.querySelector('.ion-page');
-}
+  }
 
-canDismiss = async () => {
-  const actionSheet = await this.actionSheetCtrl.create({
-    header: 'Are you sure?',
-    buttons: [
-      {
-        text: 'Yes',
-        role: 'confirm',
-      },
-      {
-        text: 'No',
-        role: 'cancel',
-      },
-    ],
-  });
+  modalOpenDelete = false;
+  modalOpenAdd = false;
+  modalOpenEdit = false;
 
-  actionSheet.present();
+  setOpenDelete(isOpen: any) {
+    this.modalOpenDelete = isOpen;
+  }
+  setOpenAdd(isOpen: any) {
+    this.modalOpenAdd = isOpen;
+  }
+  setOpenEdit(isOpen: any) {
+    this.modalOpenEdit = isOpen;
+  }
 
-  const { role } = await actionSheet.onWillDismiss();
-
-  return role === 'confirm';
-};
-
-// modalOpenDelete = false;
-modalOpenAdd = false;
-modalOpenEdit= false;
-
-// setOpenDelete(isOpen: any) {
-//   this.modalOpenDelete = isOpen;
-// }
-setOpenAdd(isOpen: any) {
-  this.modalOpenAdd = isOpen;
-}
-setOpenEdit(isOpen: any) {
-  this.modalOpenEdit= isOpen;
-}
+  console(texto: string){
+    console.log(texto);
+  }
 }
