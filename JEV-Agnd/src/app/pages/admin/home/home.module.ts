6f249b44784ab3b0396141cc68a40d4aa9535b50
MarlonPassos17 <galeriademarlon@gmail.com>
2023-08-26 14:55:23 -0300

    movendo homepage for admin page

--- JEV-Agnd/src/app/pages/admin/home/home.module.ts
+++ JEV-Agnd/src/app/pages/admin/home/home.module.ts
@@ -1,4 +1,4 @@
-import { HeaderMenuModule } from '../components/header-menu/header-menu.module'; 
+import { HeaderMenuModule } from '../../../components/header-menu/header-menu.module'; 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
