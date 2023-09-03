b01e32620a6658a46e293a1aee77bb6da5bb94d6
MarlonPassos17 <galeriademarlon@gmail.com>
2023-09-03 13:12:03 -0300

    alguns ajustes finais

--- JEV-Agnd/src/app/pages/admin/servicos/servicos.page.ts
+++ JEV-Agnd/src/app/pages/admin/servicos/servicos.page.ts
@@ -17,49 +17,49 @@ interface CardDados {
   styleUrls: ['./servicos.page.scss'],
 })
 export class ServicosPage implements OnInit {
-  private baseApiUrl = environment.baseApiUrl;
-  private apiUrl = `${this.baseApiUrl}api/servicos`;
-
+  ngOnInit(): void { }
   @Input() Card_Dados: CardDados[] = [];
-
   CardDados: any[] = [
     {
       tituloCard: 'Unhas',
       descricaoCard: 'Um ótimo serviço para quem tem dinheiro, e vontade de ficar lindo.',
-      duracaoCard: '1h',
+      duracaoCard: '60',
       precoCard: '34.00',
     },
     {
       tituloCard: 'Cabelo',
       descricaoCard: 'Um ótimo serviço para quem tem dinheiro, e vontade de ficar lindo.',
-      duracaoCard: '2h',
+      duracaoCard: '120',
       precoCard: '40.00',
     },
     {
       tituloCard: 'Sombrancelhas',
       descricaoCard: 'Um ótimo serviço para quem tem dinheiro, e vontade de ficar lindo.',
-      duracaoCard: '30mins',
+      duracaoCard: '30',
       precoCard: '30.00',
     },
     {
       tituloCard: 'Sombrancelhas',
       descricaoCard: 'Um ótimo serviço para quem tem dinheiro, e vontade de ficar lindo.',
-      duracaoCard: '30mins',
+      duracaoCard: '30',
       precoCard: '30.00',
     },
     {
       tituloCard: 'Sombrancelhas',
       descricaoCard: 'Um ótimo serviço para quem tem dinheiro, e vontade de ficar lindo.',
-      duracaoCard: '30mins',
+      duracaoCard: '30',
       precoCard: '30.00',
     },
     {
       tituloCard: 'Pés',
       descricaoCard: 'Um ótimo serviço para quem tem dinheiro, e vontade de ficar lindo.',
-      duracaoCard: '1h',
+      duracaoCard: '60',
       precoCard: '35.00',
     }
   ];
+
+  private baseApiUrl = environment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}api/servicos`;
 
   constructor(private http: HttpClient) { }
   createServico(formData: FormData): Observable<FormData> {
@@ -67,14 +67,8 @@ export class ServicosPage implements OnInit {
   }
 
   AddForm!: FormGroup;
-  EditForm!: FormGroup;
-
   modalOpenAdd = false;
-  modalOpenEdit = false;
   modalCloseAdd = false;
-  modalCloseEdit = false;
-  editFormValue: any = [];
-
   async createHandler(event: any) {
     if (this.AddForm.invalid) {
       return;
@@ -90,8 +84,7 @@ export class ServicosPage implements OnInit {
 
     this.createServico(formData).subscribe();
   }
-
-  ngOnInit(): void {
+  createFormAdd() {
     this.AddForm = new FormGroup({
       id: new FormControl(''),
       titulo: new FormControl('', [Validators.required]),
@@ -103,30 +96,6 @@ export class ServicosPage implements OnInit {
       ]))
     });
   }
-
-  createFormEdit(){
-    this.EditForm = new FormGroup({
-      id_edit: new FormControl(this.editFormValue ? this.editFormValue[0].idCard : ''),
-      titulo_edit: new FormControl(this.editFormValue ? this.editFormValue[0].tituloCard : '', [Validators.required]),
-      descricao_edit: new FormControl(this.editFormValue ? this.editFormValue[0].descricaoCard : '', [Validators.required]),
-      duracao_edit: new FormControl(this.editFormValue ? this.editFormValue[0].duracaoCard : '', [Validators.required]),
-      preco_edit: new FormControl(this.editFormValue ? this.editFormValue[0].precoCard : '', [Validators.required])
-    });
-  }
-
-  get titulo_edit() {
-    return this.EditForm.get('titulo_edit')!;
-  }
-  get descricao_edit() {
-    return this.EditForm.get('descricao_edit')!;
-  }
-  get duracao_edit() {
-    return this.EditForm.get('duracao_edit')!;
-  }
-  get preco_edit() {
-    return this.EditForm.get('preco_edit')!;
-  }
-
   get titulo() {
     return this.AddForm.get('titulo')!;
   }
@@ -139,12 +108,54 @@ export class ServicosPage implements OnInit {
   get preco() {
     return this.AddForm.get('preco')!;
   }
-
   submit_add() {
     if (this.AddForm.invalid) {
+      console.log('Formulario De Adição Invalido')
       return;
     }
-    console.log(this.AddForm.value);
+    console.log('Formulario De Adição Concluído')
+    this.CardDados.push({
+      tituloCard: this.AddForm.value.titulo,
+      descricaoCard: this.AddForm.value.descricao,
+      duracaoCard: this.AddForm.value.duracao,
+      precoCard: this.AddForm.value.preco,
+    });
+  }
+  setOpenAdd(isOpen: any) {
+    if (isOpen == true || this.AddForm.invalid && isOpen == false || this.AddForm.valid && isOpen == false) {
+      this.modalCloseAdd = !isOpen;
+      this.modalOpenAdd = isOpen;
+    }
+
+    if (isOpen == true) {
+      this.createFormAdd()
+    }
+  }
+
+  EditForm!: FormGroup;
+  editFormValue: any = [];
+  modalOpenEdit = false;
+  modalCloseEdit = false;
+  createFormEdit() {
+    this.EditForm = new FormGroup({
+      id_edit: new FormControl(this.editFormValue ? this.editFormValue[0].idCard : ''),
+      titulo_edit: new FormControl(this.editFormValue ? this.editFormValue[0].tituloCard : '', [Validators.required]),
+      descricao_edit: new FormControl(this.editFormValue ? this.editFormValue[0].descricaoCard : '', [Validators.required]),
+      duracao_edit: new FormControl(this.editFormValue ? this.editFormValue[0].duracaoCard : '', [Validators.required]),
+      preco_edit: new FormControl(this.editFormValue ? this.editFormValue[0].precoCard : '', [Validators.required])
+    });
+  }
+  get titulo_edit() {
+    return this.EditForm.get('titulo_edit')!;
+  }
+  get descricao_edit() {
+    return this.EditForm.get('descricao_edit')!;
+  }
+  get duracao_edit() {
+    return this.EditForm.get('duracao_edit')!;
+  }
+  get preco_edit() {
+    return this.EditForm.get('preco_edit')!;
   }
   submit_edit() {
     if (this.EditForm.invalid) {
@@ -159,14 +170,6 @@ export class ServicosPage implements OnInit {
       precoCard: this.EditForm.value.preco_edit,
     };
   }
-
-  setOpenAdd(isOpen: any) {
-    if (isOpen == true || this.AddForm.invalid && isOpen == false || this.AddForm.valid && isOpen == false) {
-      this.modalCloseAdd = !isOpen;
-      this.modalOpenAdd = isOpen;
-    }
-  }
-
   setOpenEdit(isOpen: any) {
     if (isOpen == true || this.EditForm.invalid && isOpen == false || this.EditForm.valid && isOpen == false) {
       this.modalCloseEdit = !isOpen;
