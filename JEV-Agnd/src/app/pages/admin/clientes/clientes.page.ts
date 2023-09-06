b321630213f8614f5f05e64e1b7e0d0a19198b43
MarlonPassos17 <galeriademarlon@gmail.com>
2023-09-05 21:51:20 -0300

    alguns ajustes

--- JEV-Agnd/src/app/pages/admin/clientes/clientes.page.ts
+++ JEV-Agnd/src/app/pages/admin/clientes/clientes.page.ts
@@ -19,53 +19,54 @@ export class ClientesPage implements OnInit {
     nome: null,
     tel: null
   }
-  
 
-  cad_cli(){
+
+  cad_cli() {
     console.log("laura")
+    // fetch('http://filhodonordeste.lovestoblog.com/?#')
     fetch('http://127.0.0.1/aula/php/')
-    .then(response => response.json())
-    // .then(response => console.log(response))
-    .then(resposta => {
-      // tranforma o json em string
-      // localStorage.setItem('nome_cliente', JSON.stringify(resposta.nome))
-      // localStorage.setItem('tel_cliente', JSON.stringify(resposta.tel))
+      .then(response => response.json())
+      // .then(response => console.log(response))
+      .then(resposta => {
+        // tranforma o json em string
+        // localStorage.setItem('nome_cliente', JSON.stringify(resposta.nome))
+        // localStorage.setItem('tel_cliente', JSON.stringify(resposta.tel))
 
-      // coloca cliente dentro do array ClientesCadastrados
-      resposta.forEach((element: any) => {
-        console.log(resposta.nome)
-        this.ClientesCadastrados.push({
-        ClienteNome: element.nome,
-        ClienteEmail: element.email,
-        ClienteTelefone: element.tel
+        // coloca cliente dentro do array ClientesCadastrados
+        resposta.forEach((element: any) => {
+          console.log(resposta.nome)
+          this.ClientesCadastrados.push({
+            ClienteNome: element.nome,
+            ClienteEmail: element.email,
+            ClienteTelefone: element.tel
+          })
+        });
+
+        console.log(resposta)
+
+        // console.log((localStorage.getItem('nome_cliente')))
+        // console.log((localStorage.getItem('tel_cliente')))
       })
+
+      .catch(error => {
+        console.log(error)
+      })
+      .finally(() => {
+        console.log("Processo Finalizado")
       });
-
-      console.log(resposta)
-
-      // console.log((localStorage.getItem('nome_cliente')))
-      // console.log((localStorage.getItem('tel_cliente')))
-    })
-
-    .catch(error => {
-      console.log(error)
-    })
-    .finally(() => {
-      console.log("Processo Finalizado")
-    });
 
   }
 
   constructor() { }
 
   // teste para integração com PHP
-  criarCliente(dados: any){
+  criarCliente(dados: any) {
     this.cliente.nome = dados.nome;
     this.cliente.tel = dados.tel;
   }
 
-  
-  exibir(){
+
+  exibir() {
     console.log('oi')
   }
 
