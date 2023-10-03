7378748eb21db4d37036ec228ab8fd88374c5e2b
nomloki <luizffreitas2006@gmail.com>
2023-10-02 23:13:48 -0300

    tirando finalmente o botão info da tela agendamentos (home)

--- JEV-Agnd/src/app/services/mensagens/msg-definidas.service.ts
+++ JEV-Agnd/src/app/services/mensagens/msg-definidas.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Clientes } from 'src/app/models/clientes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MsgDefinidasService {
 
+  private readonly API = 'http://localhost/aula/php/admin/clientes/';
+
+  
   constructor() { }
 }
