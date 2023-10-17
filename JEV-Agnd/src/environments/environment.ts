a7fb6450698c23d044cbb5a209e4e7c7722530c3
MarlonPassos17 <galeriademarlon@gmail.com>
2023-10-17 20:23:52 -0300

    alguns ajustes

--- JEV-Agnd/src/environments/environment.ts
+++ JEV-Agnd/src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   // url do Jão:
-  baseApiUrl: 'http://localhost/aula/php/admin/',
-  baseApiUrlLogin: 'http://localhost/aula/php/',
+  // baseApiUrl: 'http://localhost/aula/php/admin/',
+  // baseApiUrlLogin: 'http://localhost/aula/php/',
 
   // url do Marlon:
-  // baseApiUrl: 'http://arquivosdaaulaapi/php/admin/',
-  // baseApiUrlLogin: 'http://arquivosdaaulaapi/php/'
+  baseApiUrl: 'http://arquivosdaaulaapi/php/admin/',
+  baseApiUrlLogin: 'http://arquivosdaaulaapi/php/'
 
   // url do pc paleolítico da etec:
   // baseApiUrl: 'http://localhost/aula/php/admin/',
