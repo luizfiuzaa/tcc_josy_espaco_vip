d43fe7fa3cd3ba6d459e4331a21749b8b980515a
Marlon e Laura <134780074+MarlonPassos17@users.noreply.github.com>
2023-10-19 11:43:46 +0000

    .

--- JEV-Agnd/src/app/models/lembretes.ts
+++ JEV-Agnd/src/app/models/lembretes.ts
@@ -1,6 +1,7 @@
 export interface Lembretes {
     id: string,
     horarioLembrete: string,
+    horario: string,
     dataLembrete: string,
     conteudoLembrete: string
 }
--- JEV-Agnd/src/environments/environment.ts
+++ JEV-Agnd/src/environments/environment.ts
@@ -5,20 +5,21 @@
 export const environment = {
   production: false,
   // url do Jão:
-  baseApiUrl: 'http://localhost/aula/php/admin/',
-  baseApiUrlLogin: 'http://localhost/aula/php/',
+  // baseApiUrl: 'http://localhost/aula/php/admin/',
+  // baseApiUrlLogin: 'http://localhost/aula/php/',
 
   // url do Marlon:
   // baseApiUrl: 'http://arquivosdaaulaapi/php/admin/',
   // baseApiUrlLogin: 'http://arquivosdaaulaapi/php/'
 
   // url do pc paleolítico da etec:
-  // baseApiUrl: 'http://localhost/aula/php/admin/',
-  // baseApiUrlLogin: 'http://localhost/aula/php/'
+  baseApiUrl: 'http://localhost/aula/php/admin/',
+  baseApiUrlLogin: 'http://localhost/aula/php/'
 
   // url do codeSpace de Marlon:
   // baseApiUrl: 'http://localhost/arquivosdaaulaapi/php/admin/',
   // baseApiUrlLogin: 'http://localhost/arquivosdaaulaapi/php/'
+
 };
 
 /*
