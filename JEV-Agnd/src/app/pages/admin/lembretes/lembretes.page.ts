fc371c02e0b2bb28e3b01faeaf926056a40b188b
MarlonVBP <marlonvicctor13@gmail.com>
2023-10-31 22:57:24 -0300

    .

--- JEV-Agnd/src/app/pages/admin/lembretes/lembretes.page.ts
+++ JEV-Agnd/src/app/pages/admin/lembretes/lembretes.page.ts
@@ -3,40 +3,43 @@ import { Lembretes } from 'src/app/models/lembretes';
 import { LembretesService } from 'src/app/services/lembretes/lembretes.service';
 
 @Component({
-  selector: 'app-lembretes',
-  templateUrl: './lembretes.page.html',
-  styleUrls: ['./lembretes.page.scss'],
+	selector: 'app-lembretes',
+	templateUrl: './lembretes.page.html',
+	styleUrls: ['./lembretes.page.scss'],
 })
 export class LembretesPage implements OnInit {
-  Lembretes: Lembretes[] = [];
-  Lembretes_exibidos: Lembretes[] = [];
+	Lembretes: Lembretes[] = [];
+	Lembretes_exibidos: Lembretes[] = [];
 
-  ngOnInit() {
-  };
+	ngOnInit() {
+	};
 
-  constructor(private LembretesService: LembretesService) {
-    this.list_lembretes()
-  };
+	constructor(private LembretesService: LembretesService) {
+		this.list_lembretes()
+	};
 
-  list_lembretes() {
-    this.LembretesService.list().subscribe((dados: any) => {
-      if (dados.success == '1') {
-        this.Lembretes = dados.lembretes.map((dados: any) => {
-          var data: any = dados.dataLembrete.split("-");
-          data = `${data[2]}/${data[1]}/${data[0]}`;
-          let horario: any = dados.horario.substr(0, 5);
-          let horario_lembrete: any = dados.horarioLembrete.substr(0, 5);
-          return { ...dados, dataLembrete: data, horario: horario, horarioLembrete: horario_lembrete };
-        });
-        this.Lembretes_exibidos = this.Lembretes;
-        console.log(this.Lembretes)
-        return;
-      }
-      this.Lembretes = [];
-    })
-  }
+	list_lembretes() {
+		this.LembretesService.list().subscribe((dados: any) => {
+			if (dados.success == '1') {
+				this.Lembretes = dados.lembretes;
+				this.Lembretes_exibidos = this.Lembretes.filter((lembrete:any) => new Date(`${lembrete.dataLembrete}T${lembrete.horario}`) >= new Date()).map((dados: any) => {
+					var data: any = dados.dataLembrete.split("-");
+					data = `${data[2]}/${data[1]}/${data[0]}`;
+					let horario: any = dados.horario.substr(0, 5);
+					let horario_lembrete: any = dados.horarioLembrete.substr(0, 5);
+					return { ...dados, dataLembrete: data, horario: horario, horarioLembrete: horario_lembrete };
+				});
+				
+		console.log(this.Lembretes_exibidos)
+		console.log(this.Lembretes)
+		return;
+	}
+			this.Lembretes = [];
+this.Lembretes_exibidos = this.Lembretes;
+		})
+	}
 
-  remover_cliente(id: any) {
-    this.LembretesService.delete(id).subscribe(() => this.Lembretes = this.Lembretes.filter((lembrete: any) => lembrete.id !== id));
-  }
+remover_cliente(id: any) {
+	this.LembretesService.delete(id).subscribe(() => this.Lembretes = this.Lembretes.filter((lembrete: any) => lembrete.id !== id));
+}
 }
